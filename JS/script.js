1c0f32d9caf9497026e71a2f3dc3cf0ab992efdc
Alejandro <larim.cat@gmail.com>
2025-04-22 16:40:49 -0500

    se agrega el proyecto

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+
+document.getElementById('portalSarLink').addEventListener('click', function(event) {
+        event.preventDefault();
+        window.open('Sign-in.html', '_blank');
+    });
+    
